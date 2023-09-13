1a243c3a72fb0a70b3c284a861c7d3399c8e0714
uncle <unclesam2501@gmail.com>
2023-09-13 15:31:31 +0900

    fix comment

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,6 @@
-vim.cmd("luafile %") -- この行はinit.luaを再読み込みするために追加されました。実際の設定ファイルでは必要ありません。
+vim.cmd("luafile %") -- This line was added to reload init.lua. It's not needed in the actual configuration file.
 
-vim.g.chat_gpt_summary_language = "English" -- または "Japanese"
+vim.g.chat_gpt_summary_language = "English" -- or "Japanese"
 vim.g.chat_gpt_summary_api_key = "your_api_key_here"
 
 vim.api.nvim_set_keymap('n', '<leader>s', ':lua require("chat_gpt_summary").print_summary(vim.g.chat_gpt_summary_language, vim.g.chat_gpt_summary_api_key)<CR>', {noremap = true, silent = true})
