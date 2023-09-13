ec91ffc991b145894aa29831aa78e97bb4ac2e76
uncle <unclesam2501@gmail.com>
2023-09-13 17:44:50 +0900

    refactor dir

--- lua/code-explain.lua
+++ lua/code-explain.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.setup(config)
-    require("chat_gpt_summary").setup(config)
+    require("code-explain.chat_gpt_summary").setup(config)
 end
 
 function M.print_summary()
-    require("chat_gpt_summary").print_summary()
+    require("code-explain.chat_gpt_summary").print_summary()
 end
 
 return M
